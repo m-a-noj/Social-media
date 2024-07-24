da184657977721c57ebafde6ecb38c062049e2ce
m-a-noj <manojmandiya2005@gmail.com>
2024-07-24 17:47:21 +0530

    Add content data file and give default value to render in card

---
+++ src/store/contentData.jsx
@@ -0,0 +1,36 @@
+import { createContext, useReducer } from "react";
+
+export const Postlist =createContext({
+    postlist:[],
+    addPost:()=>{},
+    deletePost:()=>{}
+})
+const postListreducer=(currentValue,action)=>{
+    return currentValue;
+}
+const PostListProvider =({children})=>{
+    const [postlist,dispatchPostlist]=useReducer(postListreducer,DEFULAT_VALUE)
+    const addPost =()=>{}
+    const deletePost =()=>{}
+    return(
+        <Postlist.Provider value={{postlist,addPost,deletePost}}>
+            {children}
+        </Postlist.Provider>
+    )
+}
+export default PostListProvider;
+const DEFULAT_VALUE =[{
+    id:"1",
+    title:"manoj is good ",
+    description:"gfasdghjaksdfgh dasfgjasdjkf dasfjgh",
+    reaction:"12",
+    hastag : "#enjoy #party"
+},
+{
+    id:"2",
+    title:"manoj is good baD DDDHJUIASdyh ",
+    description:"gfsafddasfasdghjaksdfgh dasfgjasdjkf dasfjgh",
+    reaction:"122",
+    hastag :["#blodd" ,"#bloddy"]
+},
+]
